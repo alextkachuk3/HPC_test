fa0506cfd5181d7ffde679c05e4ff69618abde36
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2023-12-05 21:14:56 +0200

    Testing serial Monte Carlo algorithm on different iterations count and measure duration of execution

--- Serial/Serial.c
+++ Serial/Serial.c
@@ -1,30 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
+
+void TestMonteCarlo(int iterations)
+{
+	clock_t start = clock();
+
+	double x, y, z;
+	int count = 0;
+
+	for (int i = 0; i < iterations; i++)
+	{
+		x = (double)rand() / RAND_MAX;
+		y = (double)rand() / RAND_MAX;
+		z = sqrt((x * x) + (y * y));
+		if (z <= 1)
+		{
+			count++;
+		}
+	}
+
+	double pi = ((double)count / (double)iterations) * 4.0;
+
+	clock_t end = clock();
+
+	double duration = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+	printf("Iterations %i\nDuration %f\nPi: %f\n", iterations, duration, pi);
+}
 
 int main(int argc, char* argv[])
 {
-    int niter = 10000000;
+	srand(time(NULL));
 
-    double x, y, z;
-    int count = 0;
-
-    srand((unsigned int)time(NULL));
-
-    for (int i = 0; i < niter; i++)
-    {
-        x = (double)rand() / RAND_MAX;
-        y = (double)rand() / RAND_MAX;
-        z = sqrt((x * x) + (y * y));
-        if (z <= 1)
-        {
-            count++;
-        }
-    }
-
-    double pi = ((double)count / (double)niter) * 4.0;
-
-    printf("Pi: %f\n", pi);
-
-    return 0;
+	int iterations[] = { 100000, 1000000, 10000000, 100000000, 1000000000 };
+	for (int i = 0; i < sizeof(iterations) / sizeof(int); i++)
+	{
+		TestMonteCarlo(iterations[i]);
+	}
+	return 0;
 }
